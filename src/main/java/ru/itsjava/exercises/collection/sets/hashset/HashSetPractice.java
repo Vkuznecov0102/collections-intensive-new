5c6ee85a1268f00af7e4ca0b65da9b64f0424d90
Vitaliy <SeekerOfConfessor@gmail.com>
2021-01-07 21:59:18 +0300

    Initial commit

---
+++ src/main/java/ru/itsjava/exercises/collection/sets/hashset/HashSetPractice.java
@@ -0,0 +1,14 @@
+package ru.itsjava.exercises.collection.sets.hashset;
+
+import ru.itsjava.model.Man;
+
+import java.util.HashSet;
+
+public class HashSetPractice {
+
+    public static void main(String[] args) {
+//        Создать 2 множества от Man, добавить несколько Man
+//        и сравнить множества через equals
+
+    }
+}
---
+++ src/main/java/ru/itsjava/exercises/collection/sets/treeset/TreeSetPractice.java
@@ -0,0 +1,18 @@
+package ru.itsjava.exercises.collection.sets.treeset;
+
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
+public class TreeSetPractice {
+
+    public static void main(String[] args) {
+//Создать TreeSet
+//Добавить элементы  "1" , "4", "5", "7", "9", "3"
+//Вывести множество на экран
+//Вывести элемент больше 3 на экран
+//Вывести элемент меньше 3 на экран
+//создать множество в обратном порядке
+//возвращаем множество в котором все элементы больше или равны текущего
+
+    }
+}
